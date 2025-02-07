1c4ed020199a245603c0524f07b4d6d20da3acb9
chloe tu <chloe_tu@outlook.com>
2025-02-07 12:58:45 -0500

    inbox/settings/help commentary

--- moai/src/pages/Dashboard.jsx
+++ moai/src/pages/Dashboard.jsx
@@ -6,6 +6,20 @@ import "../styles/Dashboard.scss";
 const Dashboard = () => {
   const navigate = useNavigate();
 
+  // ✅ Show pop-up for "Help" and "Settings"
+  const handleHelpOrSettingsClick = () => {
+    alert(
+      "This part of Moai has yet to be built. I intend on making tutorial videos for FAQs and implementing a chatbot feature to help."
+    );
+  };
+
+  // ✅ Show pop-up for "Inbox"
+  const handleInboxClick = () => {
+    alert(
+      "I focused on the login authentication, IP-API, database structure/functionality, and Meet Someone feature for this project. There is a basic messaging system built in the backend using the database (for now), but I've yet to create the front end for it."
+    );
+  };
+
   return (
     <>
       <Header />
@@ -34,14 +48,14 @@ const Dashboard = () => {
             >
               Profile
             </Link>
-            <Link
-              to="/inbox"
+            <button
               className="dashboard__button"
               aria-label="Go to Inbox"
               role="menuitem"
+              onClick={handleInboxClick}
             >
               Inbox
-            </Link>
+            </button>
             <Link
               to="/meet-someone-new"
               className="dashboard__button"
@@ -50,14 +64,14 @@ const Dashboard = () => {
             >
               Meet Someone New
             </Link>
-            <Link
-              to="/settings-and-help"
+            <button
               className="dashboard__button"
               aria-label="Get Help"
               role="menuitem"
+              onClick={handleHelpOrSettingsClick}
             >
               Help
-            </Link>
+            </button>
 
             {/* Settings and Log Out Buttons */}
             <div
@@ -65,13 +79,13 @@ const Dashboard = () => {
               role="group"
               aria-label="Account Actions"
             >
-              <Link
-                to="/settings-and-help"
+              <button
                 className="dashboard__split-button--top"
                 aria-label="Go to Settings"
+                onClick={handleHelpOrSettingsClick}
               >
                 Settings
-              </Link>
+              </button>
               <button
                 className="dashboard__split-button--bottom"
                 onClick={() => {
